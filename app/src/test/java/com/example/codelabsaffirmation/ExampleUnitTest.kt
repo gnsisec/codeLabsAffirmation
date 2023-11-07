4cd74a15690f00a81908900cdbe71a50d599ce3a
mattivityroom <mattivityroom@gmail.com>
2023-11-07 22:38:58 +0700

    Adding instrumented test and unit test

--- app/src/test/java/com/example/codelabsaffirmation/ExampleUnitTest.kt
+++ app/src/test/java/com/example/codelabsaffirmation/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.codelabsaffirmation
-
+import android.content.Context
+import com.example.codelabsaffirmation.adapter.ItemAdapter
+import com.example.codelabsaffirmation.model.Affirmation
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.mock
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,15 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val context = mock(Context::class.java)
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation1, R.drawable.image1)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
     }
 }
